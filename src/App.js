60b16b38890e8d0263b35456ff8442b1d00e6a0f
asang <asangtamgadege70@gmail.com>
2022-09-13 11:30:10 +0530

    commit `` with '#' will be ignored, and an empty message aborts the commit.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import react, {useState} from 'react'
+import {useState} from 'react'
 import Datafilter from './dataFilter/Datafilter';
-import PropTypes from 'prop-types';
+
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
 import Divider from '@mui/material/Divider';
 import Drawer from '@mui/material/Drawer';
 import IconButton from '@mui/material/IconButton';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import MailIcon from '@mui/icons-material/Mail';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import AccounIcon from '@mui/icons-material/AccountBox';
 import MenuIcon from '@mui/icons-material/Menu';
 import './App.css';
@@ -33,32 +30,32 @@ function App(props) {
 
   const drawer = (
     <div>
-      <Toolbar />
+      <Toolbar  sx={{background:'gray'}} />
       <Divider />
       <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem key={text} disablePadding>
+      
+          <ListItem  disablePadding>
             <ListItemButton>
               <ListItemIcon>
-                {index % 2 === 0 ? <AccounIcon /> : <AccounIcon />}
+              <AccounIcon />
               </ListItemIcon>
-              <ListItemText primary={text} />
+           
             </ListItemButton>
           </ListItem>
-        ))}
+       
       </List>
       <Divider />
       <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem key={text} disablePadding>
+       
+          <ListItem disablePadding>
             <ListItemButton>
               <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <AccounIcon/>}
+              <AccounIcon />
               </ListItemIcon>
-              <ListItemText primary={text} />
+              <ListItemText  />
             </ListItemButton>
           </ListItem>
-        ))}
+
       </List>
     </div>
   );
@@ -74,7 +71,7 @@ function App(props) {
         position="fixed"
         sx={{
           width: { sm: `calc(100% - ${drawerWidth}px)` },
-          ml: { sm: `${drawerWidth}px` },
+          ml: { sm: `${drawerWidth}px` }, background:'#042f60'
         }}
       >
       
