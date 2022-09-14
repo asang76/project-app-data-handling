a656c8bc2d00e12e5a27fd25667801e1ee396568
asang <asangtamgadege70@gmail.com>
2022-09-14 09:54:01 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,130 +1,18 @@
 import {useState} from 'react'
-import Datafilter from './dataFilter/Datafilter';
 
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import Divider from '@mui/material/Divider';
-import Drawer from '@mui/material/Drawer';
-import IconButton from '@mui/material/IconButton';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import Toolbar from '@mui/material/Toolbar';
-import AccounIcon from '@mui/icons-material/AccountBox';
-import MenuIcon from '@mui/icons-material/Menu';
+import Datafilter from './dataFilter/Datafilter';
 import './App.css';
 
-const drawerWidth = 240;
+
 
 function App() {
-  
-  
-  const [mobileOpen, setMobileOpen] = useState(false);
+(
+<div>
+  <Datafilter/>
 
-  const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
-  };
-
-  const drawer = (
-    <div>
-      <Toolbar  sx={{background:'gray'}} />
-      <Divider />
-      <List>
+</div>
+    
       
-          <ListItem  disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-              <AccounIcon />
-              </ListItemIcon>
-           
-            </ListItemButton>
-          </ListItem>
-       
-      </List>
-      <Divider />
-      <List>
-       
-          <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-              <AccounIcon />
-              </ListItemIcon>
-              <ListItemText  />
-            </ListItemButton>
-          </ListItem>
-
-      </List>
-    </div>
-  );
-
-  const container = window !== undefined ? () => window().document.body : undefined;
-
-  return (
-
-    <div className='App'>
-       <Box sx={{ display: 'flex',background:'#042f60' }}>
-      <CssBaseline />
-      <AppBar
-        position="fixed"
-        sx={{
-          width: { sm: `calc(100% - ${drawerWidth}px)` },
-          ml: { sm: `${drawerWidth}px` }, background:'#042f60'
-        }}
-      >
-      
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            sx={{ display: { sm: 'none' } }}
-          >
-         <MenuIcon/>
-          </IconButton>
-       
-        
-      </AppBar>
-      <Box
-        component="nav"
-        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 },background:'#042f60' }}
-        aria-label="mailbox folders"
-      >
-        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-        <Drawer
-          container={container}
-          variant="temporary"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
-          sx={{
-            display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-          }}
-        >
-          {drawer}
-        </Drawer>
-        <Drawer
-          variant="permanent"
-          sx={{
-            display: { xs: 'none', sm: 'block' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-          }}
-          open
-        >
-          {drawer}
-        </Drawer>
-      </Box>
-      <Box container>
-       
-       <Datafilter/>
-      </Box>
-    </Box>
-    </div>
     
   );
 }
